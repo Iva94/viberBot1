a888321eb9991cd0e27d6d73d5dbb3a2755662ae
Ivana Jovovic <ivana.jovovic94@gmail.com>
2017-07-21 12:26:01 +0200

    Webhook test

--- viberbot/src/main/java/com/one/viberbot/ViberbotApplication.java
+++ viberbot/src/main/java/com/one/viberbot/ViberbotApplication.java
@@ -1,12 +1,43 @@
 package com.one.viberbot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+
+import com.google.common.util.concurrent.Futures;
+import com.viber.bot.api.ViberBot;
+import com.viber.bot.message.TextMessage;
+
+import java.util.Optional;
+
+import javax.inject.Inject;
 
 @SpringBootApplication
-public class ViberbotApplication {
+public class ViberbotApplication implements ApplicationListener<ApplicationReadyEvent>{
 
+	@Inject
+	private ViberBot bot;
+	
+	@Value("${application.viber-bot.webhook-url}")
+	private String webhookUrl;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ViberbotApplication.class, args);
 	}
+	
+	@Override
+	public void onApplicationEvent(ApplicationReadyEvent appReadyEvent) {
+        try {
+            bot.setWebhook(webhookUrl).get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        bot.onMessageReceived((event, message, response) -> response.send(message));
+        
+        bot.onConversationStarted(event -> Futures.immediateFuture(Optional.of( 
+                new TextMessage("Hi " + event.getUser().getName()))));
+    }
 }
--- viberbot/src/main/java/com/one/viberbot/service/ViberBotServiceImpl.java
+++ viberbot/src/main/java/com/one/viberbot/service/ViberBotServiceImpl.java
@@ -1,24 +1,28 @@
 package com.one.viberbot.service;
 
+import javax.annotation.Nonnull;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.one.viberbot.database.repository.ReservationRepository;
 import com.one.viberbot.database.repository.ViberBotRepository;
+import com.viber.bot.event.Event;
+import com.viber.bot.event.callback.OnConversationStarted;
+import com.viber.bot.event.callback.OnSubscribe;
 
 public class ViberBotServiceImpl implements ViberBotService {
 
 	@Autowired
 	private ViberBotRepository viberBotRepository;
 	
-	@Override
-	public void onConversationStarted() {
-		// TODO Auto-generated method stub
+	public void onConversationStarted(final @Nonnull OnConversationStarted listener) {
+	      //  on(Event.CONVERSATION_STARTED, listener);
 
 	}
 
-	@Override
-	public void onSubscribe() {
-		// TODO Auto-generated method stub
+
+	public void onSubscribe(final @Nonnull OnSubscribe listener) {
+        //on(Event.SUBSCRIBED, listener);
 
 	}
 
@@ -30,6 +34,20 @@ public class ViberBotServiceImpl implements ViberBotService {
 
 	@Override
 	public void onMessageReceived() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void onConversationStarted() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void onSubscribe() {
 		// TODO Auto-generated method stub
 		
 	}
