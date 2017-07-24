f3fd4c4f57d7cf240a0b89ac1dda71ab80deb64c
EDIT <nsmajic2@etf.unsa.ba>
2017-07-24 10:38:38 +0200

    ..

--- viberbot/src/main/java/com/one/viberbot/ViberbotApplication.java
+++ viberbot/src/main/java/com/one/viberbot/ViberbotApplication.java
@@ -1,15 +1,8 @@
 package com.one.viberbot;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
 
-import com.one.viberbot.service.ViberBotServiceImpl;
-import com.viber.bot.api.ViberBot;
-import javax.inject.Inject;
 
 
 @SpringBootApplication
