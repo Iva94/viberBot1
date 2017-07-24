26c1969687bac29dd9ba16e7d29e452cbd71964d
EDIT <nsmajic2@etf.unsa.ba>
2017-07-24 13:10:51 +0200

    added ReservationsController.java and reservations.html, added button to navbar

---
+++ viberbot/src/main/java/com/one/viberbot/mvc/controllers/room/ReservationsController.java
@@ -0,0 +1,27 @@
+package com.one.viberbot.mvc.controllers.room;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.one.viberbot.service.ReservationService;
+import com.one.viberbot.service.ReservationServiceImpl;
+
+
+@Controller
+@RequestMapping("/")
+
+public class ReservationsController {
+	
+	@Autowired
+	private ReservationServiceImpl reservationService;
+	
+	@RequestMapping(value = "/reservations", method = RequestMethod.GET)
+	public String reservationsView(Model model) {
+		model.addAttribute("reservation",reservationService.getAll());
+		return "reservations/reservations";
+	}
+
+}
